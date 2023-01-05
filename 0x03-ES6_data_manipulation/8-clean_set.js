63d7f2ab192c67839e78c15bdedf57ef5e0e87c5
jacksonhill507 <jacksonchurchill507@gmail.com>
2023-01-05 23:40:48 +0100

    my commit on data manipulation

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,3 +1,14 @@
+/* Create a function cleanSet
+   REturns startString and accepts two args */
 export default function cleanSet(set, startString) {
-  return typeof startString === 'string' && startString.length !== 0 ? [...set].filter((string) => string.startsWith(startString)).map((string) => string.replace(startString, '')).join('-') : '';
+  if (!startString || typeof startString !== 'string') return '';
+
+  const strList = [];
+  for (const str of set) {
+    if (typeof str === 'string' && str.startsWith(startString)) {
+      strList.push(str.slice(startString.length));
+    }
+  }
+
+  return strList.join('-');
 }
